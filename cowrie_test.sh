285a0fe4b5b0591f94a5eb30e5397d591e168b26
baileys-byte <vishanx23@gmail.com>
2021-05-07 19:11:55 +0100

    First 3 options configured

--- cowrie_test.sh
+++ cowrie_test.sh
@@ -34,10 +34,35 @@
     cp $newbanner $test_banner
     ;;
     [bB])
-    #echo $option
+    if [ -e $test_file ]; then
+            cp /dev/null $test_file
+    else
+            touch $test_file
+    fi
+    echo "Kernel_version = 5.4.0-42-generic" >> $test_file
+    echo "kernel_build_string = #46~18.04.1.Ubuntu SMP" >> $test_file
+    echo "arch = Linux-aarch64-lsb " >> $test_file
+    
+    cp $newbanner $test_banner
     ;;
     [cC])
-    #echo $option
+    if [ -e $test_file ]; then
+            cp /dev/null $test_file
+    else
+            touch $test_file
+    fi
+    echo "hostname = Linux Server 02" >> $test_file
+    echo "sensor_name = LinServ02" >> $test_file
+    echo "fake_addr = 192.168.55.254" >> $test_file
+    echo "ssh_version = OpenSSH_7.6p1, OpenSSL 1.0.2.n 7 Dec 2017" >> $test_file
+    echo "version = SSH-2.0-OpenSSH_7.6p1 Ubuntu-4ubuntu0.3" >> $test_file
+    echo "listen_endpoints = tcp:2222:interface=0.0.0.0" >> $test_file
+    echo "Internet_facing_ip = 9.9.9.9" >> $test_file
+    echo "Kernel_version = 5.4.0-42-generic" >> $test_file
+    echo "kernel_build_string = #46~18.04.1.Ubuntu SMP" >> $test_file
+    echo "arch = Linux-aarch64-lsb " >> $test_file
+
+    cp $newbanner $test_banner
     ;;
     [dD])
     #echo $option
