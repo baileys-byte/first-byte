c9fdc36c2acefcce09b59a3e4a9922c3b87ecb39
baileys-byte <vishanx23@gmail.com>
2021-05-07 17:22:27 +0100

    Option A selection test

--- cowrie_test.sh
+++ cowrie_test.sh
@@ -1,12 +1,48 @@
-#!bin/bash
-cowrie_test () {
-    echo "this is a test"
-    file="././etc/cowrie.cfg"
-    if [ -e $file ]; then
-            echo "this is added content"
+#!/bin/bash
+#cowrie_test (){
+
+   test_file="cowrie.cfg"
+   test_banner="motd.txt"
+   conf_file="././etc/cowrie.cfg"
+   ban_file="././honeyfs/etc/motd" 
+
+    echo "Choose your Cowrie Customization option:"
+    echo "A. Data Masking"
+    echo "B. Data Mimicry"
+    echo "C. Data Masking and Mimicry combination"
+    echo "D. Default configuration"
+
+    read option
+    
+    case $option in
+    [aA])
+    #echo $option
+    if [ -e $test_file ]; then
+            cp /dev/null $test_file
     else
-            echo "file does not exist"
+            touch $test_file
     fi
-}
+    ;;
+    [bB])
+    #echo $option
+    ;;
+    [cC])
+    #echo $option
+    ;;
+    [dD])
+    #echo $option
+    ;;
+    *)
+    echo -n "Please enter a valid option"
+    ;;
+esac
+
+
+   # if [ -e $file ]; then
+    #        echo "this is added content"
+    #else
+    #        echo "file does not exist"
+    #fi
+#}
 
 
