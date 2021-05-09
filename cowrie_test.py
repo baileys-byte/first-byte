08cad2e361b52ca4215f03c804d30c67175533d1
baileys-byte <vishanx23@gmail.com>
2021-05-09 03:02:38 +0100

    algorithm update

--- cowrie_test.py
+++ cowrie_test.py
@@ -1,21 +1,21 @@
 #THIS PROGRAM MAKES CHANGES TO THE COWRIE CONFIG FILE
 
 #Inputs: Configuration options (A, B, C, D)
-#Outputs: config.ctg and honeyfs/etc/motd
+#Outputs: config.cfg and honeyfs/etc/motd
 #Call the command 
 # Choose an option
 #      IF option = A, B or C Then
-#          Check for config.ctg
-#           IF config.ctg exists Then 
-#               Clear contents of config.ctg 
+#          Check for config.cfg
+#           IF config.cfg exists Then 
+#               Clear contents of config.cfg 
 #           Else
-#                Create config.ctg
+#                Create config.cfg
 #           ADD configuration settings
 #         LOCATE and EDIT banner file
 #     IF option = D Then
-#         Check for config.ctg
-#         IF config.ctg exists Then 
-#               Clear contents of config.ctg 
+#         Check for config.cfg
+#         IF config.cfg exists Then 
+#               Clear contents of config.cfg 
 #         Else
-#               Create config.ctg
+#               Create config.cfg
 #  End
